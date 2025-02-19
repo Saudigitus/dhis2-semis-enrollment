71421886aa180ffda7febce4803142730d53833c
cnandza <cnandza@saudigitus.org>
2025-02-19 10:25:00 +0200

    chore: update package version and simplify routing structure by removing HashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import './App.module.css'
-import { RecoilRoot } from 'recoil'
 import { Router } from '../components/routes'
-import { AppWrapper } from 'dhis2-semis-components'
 
 const MyApp = () => {
 
     return (
-        <RecoilRoot>
-            <AppWrapper dataStoreKey='semis/values'>
-                <Router />
-            </AppWrapper>
-        </RecoilRoot>
+        <Router />
     )
 }
 
--- src/components/routes/Router.tsx
+++ src/components/routes/Router.tsx
@@ -1,103 +1,101 @@
 import React from 'react';
 import { EnrollmentsPage } from '../../pages';
-import { Routes, Route, HashRouter, Outlet } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import { HeaderBarLayout, SemisHeader } from 'dhis2-semis-components';
 
 export default function Router() {
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/'
-                    element={
-                        <HeaderBarLayout
-                            header={<SemisHeader
-                                headerItems={{
-                                    academicYears: {
-                                        options: [
-                                            {
-                                                label: '2024',
-                                                value: '2024'
-                                            },
-                                            {
-                                                label: '2023',
-                                                value: '2023'
-                                            },
-                                            {
-                                                label: '2022',
-                                                value: '2022'
-                                            }
-                                        ]
-                                    },
-                                    classes: {
-                                        options: [
-                                            {
-                                                label: 'A',
-                                                value: 'A'
-                                            },
-                                            {
-                                                label: 'B',
-                                                value: 'B'
-                                            },
-                                            {
-                                                label: 'C',
-                                                value: 'C'
-                                            },
-                                            {
-                                                label: 'D',
-                                                value: 'D'
-                                            },
-                                            {
-                                                label: 'E',
-                                                value: 'E'
-                                            },
-                                            {
-                                                label: 'F',
-                                                value: 'F'
-                                            }
-                                        ]
-                                    },
-                                    grades: {
-                                        options: [
-                                            {
-                                                label: 'Grade 1',
-                                                value: 'Grade 1'
-                                            },
-                                            {
-                                                label: 'Grade 2',
-                                                value: 'Grade 2'
-                                            },
-                                            {
-                                                label: 'Grade 3',
-                                                value: 'Grade 3'
-                                            },
-                                            {
-                                                label: 'Grade 4',
-                                                value: 'Grade 4'
-                                            },
-                                            {
-                                                label: 'Grade 5',
-                                                value: 'Grade 5'
-                                            },
-                                            {
-                                                label: 'Grade 6',
-                                                value: 'Grade 6'
-                                            },
-                                            {
-                                                label: 'Grade 7',
-                                                value: 'Grade 7'
-                                            }
-                                        ]
-                                    },
-                                    orgunits: {
-                                        options: []
-                                    }
-                                }} />}>
-                            <Outlet />
-                        </HeaderBarLayout>}
-                >
-                    <Route key={'enrollments'} path={'/enrollments'} element={<EnrollmentsPage />} />
-                </Route>
-            </Routes>
-        </HashRouter>
+        <Routes>
+            <Route path='/'
+                element={
+                    <HeaderBarLayout
+                        header={<SemisHeader
+                            headerItems={{
+                                academicYears: {
+                                    options: [
+                                        {
+                                            label: '2024',
+                                            value: '2024'
+                                        },
+                                        {
+                                            label: '2023',
+                                            value: '2023'
+                                        },
+                                        {
+                                            label: '2022',
+                                            value: '2022'
+                                        }
+                                    ]
+                                },
+                                classes: {
+                                    options: [
+                                        {
+                                            label: 'A',
+                                            value: 'A'
+                                        },
+                                        {
+                                            label: 'B',
+                                            value: 'B'
+                                        },
+                                        {
+                                            label: 'C',
+                                            value: 'C'
+                                        },
+                                        {
+                                            label: 'D',
+                                            value: 'D'
+                                        },
+                                        {
+                                            label: 'E',
+                                            value: 'E'
+                                        },
+                                        {
+                                            label: 'F',
+                                            value: 'F'
+                                        }
+                                    ]
+                                },
+                                grades: {
+                                    options: [
+                                        {
+                                            label: 'Grade 1',
+                                            value: 'Grade 1'
+                                        },
+                                        {
+                                            label: 'Grade 2',
+                                            value: 'Grade 2'
+                                        },
+                                        {
+                                            label: 'Grade 3',
+                                            value: 'Grade 3'
+                                        },
+                                        {
+                                            label: 'Grade 4',
+                                            value: 'Grade 4'
+                                        },
+                                        {
+                                            label: 'Grade 5',
+                                            value: 'Grade 5'
+                                        },
+                                        {
+                                            label: 'Grade 6',
+                                            value: 'Grade 6'
+                                        },
+                                        {
+                                            label: 'Grade 7',
+                                            value: 'Grade 7'
+                                        }
+                                    ]
+                                },
+                                orgunits: {
+                                    options: []
+                                }
+                            }} />}>
+                        <Outlet />
+                    </HeaderBarLayout>}
+            >
+                <Route key={'enrollments'} path={'/'} element={<EnrollmentsPage />} />
+            </Route>
+        </Routes>
     );
 }
