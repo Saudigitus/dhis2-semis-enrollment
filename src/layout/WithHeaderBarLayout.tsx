5474640d006806efc24f3b541729947427a5e4fd
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-05-28 15:11:13 +0200

    feat: enhaced semis header

--- src/layout/WithHeaderBarLayout.tsx
+++ src/layout/WithHeaderBarLayout.tsx
@@ -1,124 +1,120 @@
 import { Outlet } from "react-router-dom"
-import { HeaderBarLayout, SemisHeader } from "dhis2-semis-components"
+import { HeaderBarLayout, SemisHeader, useProgramsKeys } from "dhis2-semis-components"
 
 const WithHeaderBarLayout = () => {
-
+    const [program] = useProgramsKeys()
     return (
         <HeaderBarLayout
             header={
                 <SemisHeader
-                    headerItems={{
-                        // employmentType: {
-                        //     options: [
-                        //         {
-                        //             label: '2024',
-                        //             value: '2024'
-                        //         },
-                        //         {
-                        //             label: '2023',
-                        //             value: '2023'
-                        //         },
-                        //         {
-                        //             label: '2022',
-                        //             value: '2022'
-                        //         }
-                        //     ]
-                        // },
-                        // typeOfStaff: {
-                        //     options: [
-                        //         {
-                        //             label: '2024',
-                        //             value: '2024'
-                        //         },
-                        //         {
-                        //             label: '2023',
-                        //             value: '2023'
-                        //         },
-                        //         {
-                        //             label: '2022',
-                        //             value: '2022'
-                        //         }
-                        //     ]
-                        // },
-                        academicYears: {
-                            options: [
+                    program={program}
+                    dataSoreValues={{
+                        "key": "student",
+                        "filters": {
+                            "dataElements": [
                                 {
-                                    label: '2024',
-                                    value: '2024'
+                                    "label": "grade",
+                                    "ulrParam": "grade",
+                                    "dataElement": "kNNoif9gASf",
                                 },
                                 {
-                                    label: '2023',
-                                    value: '2023'
-                                },
-                                {
-                                    label: '2022',
-                                    value: '2022'
+                                    "label": "class",
+                                    "ulrParam": "section",
+                                    "dataElement": "RhABRLO2Fae"
                                 }
                             ]
                         },
-                        classes: {
-                            options: [
+                        "modules": [
+                            {
+                                "key": "attendance",
+                                "name": "Attendance",
+                                "display": true
+                            },
+                            {
+                                "key": "absenteeism",
+                                "name": "Absenteeism",
+                                "display": false
+                            },
+                            {
+                                "key": "performance",
+                                "name": "Performance",
+                                "display": true
+                            }
+                        ],
+                        "program": "wQaiD2V27Dp",
+                        "defaults": {
+                            "defaultOrder": "gz8w04YBSS0:asc",
+                            "allowSearching": true,
+                            "currentAcademicYear": "2024"
+                        },
+                        "transfer": {
+                            "status": "YnwITieplwy",
+                            "programStage": "uewAr6TmLkw",
+                            "destinySchool": "kQbquG7UivM",
+                            "statusOptions": [
                                 {
-                                    label: 'A',
-                                    value: 'A'
+                                    "key": "pending",
+                                    "code": "Pending"
                                 },
                                 {
-                                    label: 'B',
-                                    value: 'B'
+                                    "key": "approved",
+                                    "code": "Approved"
                                 },
                                 {
-                                    label: 'C',
-                                    value: 'C'
-                                },
-                                {
-                                    label: 'D',
-                                    value: 'D'
-                                },
-                                {
-                                    label: 'E',
-                                    value: 'E'
-                                },
-                                {
-                                    label: 'F',
-                                    value: 'F'
+                                    "key": "reproved",
+                                    "code": "Reproved"
                                 }
                             ]
                         },
-                        grades: {
-                            options: [
+                        "attendance": {
+                            "status": "d0MKWRNGv0a",
+                            "lastUpdate": "2024-10-17 12:14:41",
+                            "programStage": "Ljyrr3cktAr",
+                            "absenceReason": "oLUMMT84ILM",
+                            "statusOptions": [
                                 {
-                                    label: 'Grade 1',
-                                    value: 'Grade 1'
+                                    "key": "absent",
+                                    "code": "absent",
+                                    "icon": "correct_blue_fill",
+                                    "color": "#E57373"
                                 },
                                 {
-                                    label: 'Grade 2',
-                                    value: 'Grade 2'
-                                },
-                                {
-                                    label: 'Grade 3',
-                                    value: 'Grade 3'
-                                },
-                                {
-                                    label: 'Grade 4',
-                                    value: 'Grade 4'
-                                },
-                                {
-                                    label: 'Grade 5',
-                                    value: 'Grade 5'
-                                },
-                                {
-                                    label: 'Grade 6',
-                                    value: 'Grade 6'
-                                },
-                                {
-                                    label: 'Grade 7',
-                                    value: 'Grade 7'
+                                    "key": "present",
+                                    "code": "present",
+                                    "icon": "wrong_red_fill",
+                                    "color": "#81C784"
                                 }
                             ]
                         },
-                        orgunits: {
-                            options: []
-                        }
+                        "lastUpdate": "2024-10-17 12:14:41",
+                        "performance": {
+                            "programStages": [
+                                {
+                                    "programStage": "mBEhR2M4DRQ"
+                                },
+                                {
+                                    "programStage": "aDmsN3qemOA"
+                                },
+                                {
+                                    "programStage": "rZGdcch2PCh"
+                                }
+                            ]
+                        },
+                        "final-result": {
+                            "status": "bsyU0WFfskG",
+                            "programStage": "hcrjYJ6Yl5F"
+                        },
+                        "registration": {
+                            "grade": "kNNoif9gASf",
+                            "section": "RhABRLO2Fae",
+                            "lastUpdate": "2024-05-09 15:50:41",
+                            "academicYear": "iDSrFrrVgmX",
+                            "programStage": "Ni2qsy2WJn4"
+                        },
+                        "socio-economics": {
+                            "programStage": "Wi3KEZ7C3w9"
+                        },
+                        "trackedEntityType": "eMLK4VQm3Kj"
                     }}
                 />
             }
