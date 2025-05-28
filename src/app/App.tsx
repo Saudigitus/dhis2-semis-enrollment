f6add7cc20408f4325ad5e9e5fe41d22a003b925
cnandza <cnandza@saudigitus.org>
2025-05-28 10:37:32 +0200

    update wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
+import { AppWrapper } from 'dhis2-semis-components'
+import { HashRouter } from 'react-router-dom'
+import { useConfig } from '@dhis2/app-runtime'
 
 const Enrollment = () => {
+    const { baseUrl } = useConfig()
 
     return (
-        <Router />
+        <AppWrapper
+            baseUrl={baseUrl}
+            dataStoreKey='dataStore/semis/values'
+        >
+            <HashRouter>
+                <Router />
+            </HashRouter>
+        </AppWrapper>
     )
 }
 
