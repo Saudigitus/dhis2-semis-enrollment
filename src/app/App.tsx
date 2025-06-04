db0a8b3b2d13991f33b407534d254316b2a5d473
cnandza <cnandza@saudigitus.org>
2025-06-04 16:39:32 +0200

    bump version to 1.0.1-alpha.39 and simplify Enrollment component by removing unnecessary wrappers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
-import { AppWrapper } from 'dhis2-semis-components'
-import { HashRouter } from 'react-router-dom'
-import { useConfig } from '@dhis2/app-runtime'
-import InitializeWrapper from '../components/wrapper/InitializeWrapper'
 
 const Enrollment = () => {
-    const { baseUrl } = useConfig()
 
     return (
-        <AppWrapper
-            baseUrl={baseUrl}
-            dataStoreKey='dataStore/semis/values'
-        >
-            <InitializeWrapper>
-                <HashRouter>
-                    <Router />
-                </HashRouter>
-            </InitializeWrapper>
-        </AppWrapper>
+        <Router />
     )
 }
 
