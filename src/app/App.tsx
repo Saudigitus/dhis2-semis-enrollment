9f73c41b3661894669c4595e5d551b370c4439c9
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-10-31 16:07:43 +0200

    refactor(App): remove AppWrapper and direct translation usage
    Simplify the component structure by removing AppWrapper and directly using i18n prop. Also update package version to 1.0.2-alpha.9.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,22 +5,22 @@ import { useConfig } from '@dhis2/app-runtime'
 import { HashRouter } from 'react-router-dom'
 import { AppWrapper } from 'dhis2-semis-components'
 import { D2I18n } from 'dhis2-semis-types'
-import translation from '../locales/index'
+// import translation from '../locales/index'
 
 const Enrollment = ({ i18n }: { i18n: D2I18n }) => {
     const { baseUrl } = useConfig()
-    const translate = i18n ? i18n : translation
+    // const translate = i18n ? i18n : translation
 
     return (
-        <AppWrapper
-            baseUrl={baseUrl}
-            dataStoreKey="dataStore/semis/values"
-            schoolCalendarKey='dataStore/semis/schoolCalendar'
-        >
-            <HashRouter>
-                <Router i18n={translate as unknown as any} />
-            </HashRouter >
-        </AppWrapper>
+        // <AppWrapper
+        //     baseUrl={baseUrl}
+        //     dataStoreKey="dataStore/semis/values"
+        //     schoolCalendarKey='dataStore/semis/schoolCalendar'
+        // >
+        //     <HashRouter>
+                <Router i18n={i18n as unknown as any} />
+        //     </HashRouter >
+        // </AppWrapper>
     )
 }
 
